0a93aef6f0b593156a43449338d8c64d1d3b4199
Van-Ivan <pibo0121016@sibupk.su>
2022-12-17 11:59:57 +0700

    гог

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class strNSimbol
+    {
+        public string Write(int n)
+        {
+            if (n<1 || n > 26)
+            {
+                throw new ArgumentException();
+            }
+            string str = "";
+            for (char c = 'A'; c < 'A' + n; c++)
+            {
+                str = str + c;
+            }
+            return str;
+        }
+    }
+    public class QuadEquation
+    {
+        private double a, b, c;
+        public QuadEquation(double a, double b, double c)
+        {
+            if (a == 0) throw new ArgumentException();
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public double[] Solution()
+        {
+            double D = b * b - (4 * a * c);
+            if (D > 0)
+            {
+                double x1 = (-b + Math.Sqrt(D)) / 2;
+                double x2 = (-b - Math.Sqrt(D)) / 2;
+                Math.Round(x1, 2);
+                Math.Round(-x2, 2);
+                return new double[] { x1, x2 };
+            }
+            else if (D == 0)
+            {
+                double x = (-b - Math.Sqrt(D)) / 2;
+                Math.Round(x, 2);
+                return new double[] { x };
+            }
+            else
+            {
+                return new double[0];
+            }
+        }
+    }
+    public class Visokos
+    {
+        int year;
+        public Visokos(int year)
+        {
+            if (year < 1)
+            {
+                throw new ArgumentException();
+            }
+            this.year = year;
+        }
+        public int NumDays(int year)
+        {
+            if (year % 4 == 0)
+            {
+                if ((year % 100 == 0) && (year % 400 != 0))
+                    return 365;
+                else
+                    return 366;
+            }
+            else
+                return 365;
+        }
+    }
+    public class Regular
+    {
+        Regex regex = new Regex(@"^[a-zA-Z]\w{3,20}@[a-z]{2,10}\.[a-z]{2,5}");
+        public bool ISEmail(string line)
+        {
+            return regex.IsMatch(line);
+        }
+    }
+    public class Number
+    {
+        public int Sum(string str)
+        {
+            int sum = 0;
+            for (int i=0; i< str.Length; i++)
+            {
+                if (str[i] < '0' || str[i] > '9')
+                {
+                    throw new ArgumentException();
+                }
+                sum += str[i] - '0';
+            }
+            return sum;
+        }
+    }
+}
---
+++ ClassLibrary1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,205 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ClassLibrary1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class strNSymbolTest
+    {
+        [TestMethod]
+        public void strNsymbolTest1()
+        {
+            int n = 5;
+            string expected = "ABCDE";
+            strNSimbol str = new strNSimbol();
+            string actual = str.Write(n);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void strNsymbolTest2()
+        {
+            bool cathed = false;
+            try
+            {
+                int n = 27;
+                strNSimbol str = new strNSimbol();
+                string actual = str.Write(n);
+            }
+            catch (ArgumentException)
+            {
+                cathed = true;
+            }
+            
+            Assert.IsTrue(cathed, "АргументЭксэпшн");
+        }
+        [TestMethod]
+        public void strNsymbolTest3()
+        {
+            bool cathed = false;
+            try
+            {
+                int n = 0;
+                strNSimbol str = new strNSimbol();
+                string actual = str.Write(n);
+            }
+            catch (ArgumentException)
+            {
+                cathed = true;
+            }
+
+            Assert.IsTrue(cathed, "АргументЭксэпшн");
+        }
+    }
+
+    [TestClass]
+    public class QuadEquationTest
+    {
+        [TestMethod]
+        public void QuadEquationTest1()
+        {
+            double a = 1, b = 1, c = 1;
+            var eq = new QuadEquation(a, b, c);
+            double[] actual = eq.Solution();
+            double[] expected = new double[0];
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void QuadEquationTest2()
+        {
+            //1 тест ?
+            bool cathed = false;
+            try
+            {
+                double a = 0, b = 1, c = 1;
+                var eq = new QuadEquation(a, b, c);
+                double[] actual = eq.Solution();
+                double[] expected = { };
+            }
+            catch
+            {
+                cathed = true;
+            }
+            Assert.IsTrue(cathed);
+        }
+        [TestMethod]
+        public void QuadEquationTest3()
+        {
+            double a = 1, b = -6, c = 9;
+            var eq = new QuadEquation(a, b, c);
+            double[] actual = eq.Solution();
+            double[] expected = { 3 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void QuadEquationTest4()
+        {
+            double a = 1, b = 4, c = 3;
+            var eq = new QuadEquation(a, b, c);
+            double[] actual = eq.Solution();
+            double[] expected = { -1, -3};
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+    [TestClass]
+    public class VisilosTest
+    {
+        [TestMethod]
+        public void NumYearTest1()
+        {
+            int year = 300;
+            Visokos v = new Visokos(year);
+            int actual = v.NumDays(year);
+            int expected = 365;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void NumYearTest2()
+        {
+            int year = 1200;
+            Visokos v = new Visokos(year);
+            int actual = v.NumDays(year);
+            int expected = 366;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void NumYearTest3()
+        {
+            bool cathed = false;
+            try
+            {
+                int year = 0;
+                Visokos v = new Visokos(year);
+                int actual = v.NumDays(year);
+            }
+            catch (ArgumentException)
+            {
+                cathed = true;
+            }
+            Assert.IsTrue(cathed);
+        }
+    }
+    [TestClass]
+    public class RegularTest
+    {
+        Regular r = new Regular();
+        [TestMethod]
+        public void IsEmailTest1()
+        {
+            string line = "test@test.test";
+            Assert.IsTrue(r.ISEmail(line));
+        }
+        [TestMethod]
+        public void IsEmailTest2()
+        {
+            string line = "test@testtest";
+            Assert.IsFalse(r.ISEmail(line));
+        }
+    }
+    [TestClass]
+    public class NumberTest
+    {
+        string str;
+        Number a = new Number();
+        [TestMethod]
+        public void SumTest1()
+        {
+            str = "123";
+            int actual = a.Sum(str);
+            int expected = 6;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void SumTest2()
+        {
+            bool cathed = false;
+            try
+            {
+                str = "afs";
+                int actual = a.Sum(str);
+            }
+            catch (ArgumentException)
+            {
+                cathed = true;
+            }
+            
+            Assert.IsTrue(cathed);
+        }
+        [TestMethod]
+        public void SumTest3()
+        {
+            bool cathed = false;
+            try
+            {
+                str = "-1";
+                int actual = a.Sum(str);
+            }
+            catch (ArgumentException)
+            {
+                cathed = true;
+            }
+
+            Assert.IsTrue(cathed);
+        }
+    }
+}
